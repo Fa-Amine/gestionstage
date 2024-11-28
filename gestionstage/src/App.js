48f00e38c585f7aa580e9d3abaa562d7bde400f0
Fa-Amine <farah@gmail.com>
2024-11-28 13:22:46 +0100

    basic login version 1

--- gestionstage/src/App.js
+++ gestionstage/src/App.js
@@ -4,16 +4,16 @@ import { Flex,Text } from '@chakra-ui/react';
 import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
+import Login from './components/Login';
 
 function App() {
   return (
     <>
       <div className="App">
       <Header />
-      <Main />
-        {/* <div className="container" >
-          <h1>ana</h1>
-        </div> */}
+      {/* <Main /> */}
+      <Login />
+       
       </div>
     </>
   );
---
+++ gestionstage/src/components/Login.js
@@ -0,0 +1,54 @@
+import {
+    Box,
+    Flex,
+    Text,
+    IconButton,
+    Stack,
+    Collapse,
+    useColorModeValue,
+    useBreakpointValue,
+    useDisclosure,
+    Button,
+    image,
+    Container,
+    Heading,
+    Image,
+    Input,
+    Grid,
+    Checkbox
+  } from '@chakra-ui/react'
+
+  export default function Login() {
+    return (
+        
+      <Flex   justifyContent={'center'} mt="50px" padding={"50"}>
+         <Box>
+                <Image  w="90%" src="/Images/upfimg.png" />
+
+        </Box>
+        <Box
+          h="500px"
+          rounded="l3"
+          width="50%"
+          color="black"
+          justifyItems={'flex-start'}
+        >
+          <Heading ml="30px" mt="30px" mb="30px" fontSize={"30px"}>
+            Bienvenue
+          </Heading>
+          <Grid w="full" justifyItems={'flex-start'} padding={"50px"} templateColumns="repeat(2, 1fr)" >
+            <Text>Email</Text>
+            <Input mb={"5"} rounded={'3xl'} placeholder="Votre Email" />
+            <Text >Mot de passe</Text>
+            <Input rounded={'3xl'} placeholder="Votre mot de passe ici" />
+            <Flex justifyContent={"center"} gap="10px" mt="20px">
+            <Button w={"50%"}rounded={'2xl'} mt = "20px" colorPalette="cyan" variant="solid">Se connecter</Button>
+            <Button   w={"50%"} rounded={'2xl'} mt = "20px" colorPalette="cyan" variant="solid">S'inscrire</Button></Flex>
+          </Grid>
+          
+        </Box>
+      </Flex>
+    );
+
+
+  }
